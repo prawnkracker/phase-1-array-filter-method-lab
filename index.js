488690ee65e80aa7ba042af1ceac4984d77143e4
Dennis Demir <dennisjdemir@gmail.com>
2023-03-21 20:36:48 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, name){
+    return array.filter(function(item){
+        return item.toLowerCase() === name.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers,string){
+    return drivers.filter(function(item){
+        return item[0] === string[0];
+    })
+}
+
+function matchName(drivers,string) {
+    return drivers.filter(function(item){
+        return item.name === string
+    })
+}
